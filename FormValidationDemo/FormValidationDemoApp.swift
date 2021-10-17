cfbb2e2a675c9acace14b5d8b207417888adab7e
Kentaro Kamiyama <kamiken0215@gmail.com>
2021-10-17 14:44:27 +0900

    Initial Commit

---
+++ FormValidationDemo/FormValidationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormValidationDemoApp.swift
+//  FormValidationDemo
+//
+//  Created by 神山賢太郎 on 2021/10/17.
+//
+
+import SwiftUI
+
+@main
+struct FormValidationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
